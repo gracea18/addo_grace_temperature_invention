f0e48f14ccef285b19fe819d1e47cf5af566edbd
gracea18 <72771341+gracea18@users.noreply.github.com>
2020-11-25 21:53:01 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 
 while True:
     print("Temperature:" + input.temperature(TemperatureUnit.FAHRENHEIT))
-    if input.temperature(TemperatureUnit.FAHRENHEIT) > 68 and input.temperature(TemperatureUnit.FAHRENHEIT) < 74:
+    
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > 74:
+        light.clear()
+        light.set_brightness(255)
+        light.set_all(light.rgb(255,0,0))
+        light.clear()
+        light.set_all(light.rgb(255,0,0))
+        
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
+        light.clear()
+        light.set_brightness(255)
+        light.set_all(light.rgb(0,0,255))
+        light.clear()
+        light.set_all(light.rgb(0,0,255))
+       
+    else:
         light.set_brightness(100)
         light.set_pixel_color(0, light.rgb(255,128,0))
         light.set_pixel_color(1, light.rgb(255,255,0))
@@ -13,19 +28,3 @@ while True:
         light.set_pixel_color(7, light.rgb(128,0,255))
         light.set_pixel_color(8, light.rgb(255,0,128))
         light.set_pixel_color(9, light.rgb(255,0,0))
-    elif input.temperature(TemperatureUnit.FAHRENHEIT) > 74:
-        light.clear()
-        light.set_brightness(255)
-        light.set_all(light.rgb(255,0,0))
-        light.clear()
-        light.set_all(light.rgb(255,0,0))
-        light.clear()
-        light.set_all(light.rgb(255,0,0))
-    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
-        light.clear()
-        light.set_brightness(255)
-        light.set_all(light.rgb(0,0,255))
-        light.clear()
-        light.set_all(light.rgb(0,0,255))
-        light.clear()
-        light.set_all(light.rgb(0,0,255))
--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
 while (true) {
     console.log("Temperature:" + input.temperature(TemperatureUnit.Fahrenheit))
-    if (input.temperature(TemperatureUnit.Fahrenheit) > 68 && input.temperature(TemperatureUnit.Fahrenheit) < 74) {
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 74) {
+        light.clear()
+        light.setBrightness(255)
+        light.setAll(light.rgb(255, 0, 0))
+        light.clear()
+        light.setAll(light.rgb(255, 0, 0))
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
+        light.clear()
+        light.setBrightness(255)
+        light.setAll(light.rgb(0, 0, 255))
+        light.clear()
+        light.setAll(light.rgb(0, 0, 255))
+    } else {
         light.setBrightness(100)
         light.setPixelColor(0, light.rgb(255, 128, 0))
         light.setPixelColor(1, light.rgb(255, 255, 0))
@@ -12,22 +24,6 @@ while (true) {
         light.setPixelColor(7, light.rgb(128, 0, 255))
         light.setPixelColor(8, light.rgb(255, 0, 128))
         light.setPixelColor(9, light.rgb(255, 0, 0))
-    } else if (input.temperature(TemperatureUnit.Fahrenheit) > 74) {
-        light.clear()
-        light.setBrightness(255)
-        light.setAll(light.rgb(255, 0, 0))
-        light.clear()
-        light.setAll(light.rgb(255, 0, 0))
-        light.clear()
-        light.setAll(light.rgb(255, 0, 0))
-    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
-        light.clear()
-        light.setBrightness(255)
-        light.setAll(light.rgb(0, 0, 255))
-        light.clear()
-        light.setAll(light.rgb(0, 0, 255))
-        light.clear()
-        light.setAll(light.rgb(0, 0, 255))
     }
     
 }
